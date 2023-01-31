769d8f752b039a68bf3440e6f6d55347b298ac12
0xGrvty <vant6131@gmail.com>
2023-01-30 18:25:10 -0700

    Refactored a little  bit and moved some of the UI scene ontainers around

--- MF/Assets/Scripts/Core/UI/HealthBarContainer.cs
+++ MF/Assets/Scripts/Core/UI/HealthBarContainer.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public class HealthBar : Control
+public class HealthBarContainer : Control
 {
     private void OnUIHealthChanged(int health, int maxHealth) {
         GetNode<Label>("VContainer/LifeValue").Text = String.Format("{0}/{1}", health, maxHealth);

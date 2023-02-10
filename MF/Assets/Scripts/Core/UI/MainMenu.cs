8956d537d55377a64e9d43f38d1613a29b58aff9
Studio Ronin <studioroningames@gmail.com>
2023-02-09 19:56:39 -0700

    Test commit from another computer

--- MF/Assets/Scripts/Core/UI/MainMenu.cs
+++ MF/Assets/Scripts/Core/UI/MainMenu.cs
@@ -4,25 +4,25 @@ using System;
 public class MainMenu : Node2D
 {
 #pragma warning disable 649
-    [Export]
-    private PackedScene mainGame;
-    [Export]
-    private PackedScene optionsMenu;
+	[Export]
+	private PackedScene mainGame;
+	[Export]
+	private PackedScene optionsMenu;
 #pragma warning restore 649
 
-    public override void _Ready() {
-    }
+	public override void _Ready() {
+	}
 
-    public void OnNewGameButtonButtonUp() {
-        GetTree().ChangeSceneTo(mainGame);
-    }
+	public void OnNewGameButtonButtonUp() {
+		GetTree().ChangeSceneTo(mainGame);
+	}
 
-    public void OnOptionsButtonButtonUp() {
-        GetTree().ChangeSceneTo(optionsMenu);
-    }
+	public void OnOptionsButtonButtonUp() {
+		GetTree().ChangeSceneTo(optionsMenu);
+	}
 
-    public void OnExitButtonButtonUp() {
-        GetTree().Quit();
-    }
+	public void OnExitButtonButtonUp() {
+		GetTree().Quit();
+	}
 
 }
